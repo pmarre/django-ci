c3ff1a8a2df544358c62413a87a1b11d37c4775e
Patrick Marre <pmarre40@gmail.com>
2020-08-12 15:19:59 -0700

    django todo practice

---
+++ todo/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .models import Item
+
+# Create your tests here.
+
+
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test todo status')
+        self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name="Test name")
+        self.assertEqual(str(item), 'Test name')
